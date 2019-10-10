46702f5e4c93860a428da5cf8887719df89b9713
igor <x.firedragon@gmail.com>
2019-10-10 22:05:51 +0500

    init postgres in docker

---
+++ src/database/database.providers.ts
@@ -0,0 +1,19 @@
+import { createConnection } from 'typeorm';
+
+export const databaseProviders = [
+  {
+    provide: 'DATABASE_CONNECTION',
+    useFactory: async () => await createConnection({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: parseInt(process.env.POSTGRES_PORT, 10),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: 'postgres',
+      entities: [
+        __dirname + '/../**/*.entity.ts',
+      ],
+      synchronize: true,
+    }),
+  },
+];
